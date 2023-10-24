560dd3ce2e627867ab195583fed686d30f035209
Jesse Paradero <143587636+JesseParadero@users.noreply.github.com>
2023-10-24 14:04:13 +0800

    a

---
+++ View/mySubscription.php
@@ -0,0 +1,114 @@
+<?php
+include("../Model/db.php");
+session_start();
+error_reporting(0);
+
+if (!isset($_SESSION['id'])) {
+    header("Location: ../index.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <?php include("../layouts/head.layout.php") ?>
+    <link rel="stylesheet" href="../assets/css/myProduct.css">
+    <style>
+        .btn {
+            min-width: 100px;
+        }
+
+        input[type=text] {
+            border: 1px solid #000;
+            color: #000;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    $user = mysqli_fetch_assoc(getrecord('user_details', 'id', $_SESSION['id']));
+    ?>
+    <div class="page-wrapper">
+        <?php include("../layouts/header_layout.php"); ?>
+        <div class="container mt-2">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="homepage.php">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Manage Subscription</li>
+                </ol>
+            </nav>
+        </div>
+        <main class="main mt-3">
+            <div class="page-content">
+                <div class="dashboard">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <aside class="col-md-2 col-lg-2" style="border-right: 1px solid #ebebeb;">
+                                <ul class="nav nav-dashboard flex-column mb-3 mb-md-0" role="tablist"
+                                    style="height:600px;">
+                                    <li class="nav-item">
+                                        <a href="myAccount.php" class="nav-link">My Account</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="myProduct.php" class="nav-link">My Product</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="manageOrder.php" class="nav-link">Orders</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="myPurchase.php" class="nav-link">My Purchase</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="myShop.php" class="nav-link">My shop</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="mySubscription.php" class="nav-link active">Manage Subscription</a>
+                                    </li>
+                                </ul>
+                            </aside>
+                            <div class="col-10">
+                                <table class="table table-hover text-center">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>TYPE</th>
+                                            <th>Amount</th>
+                                            <th>Reference Number</th>
+                                            <th>Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <?php include("../layouts/footer.layout1.php"); ?>
+    </div>
+
+
+    <?php
+    include("../layouts/jsfile.layout.php");
+    include("toastr.php");
+    include('../assets/js/prod.php');
+    ?>
+
+</body>
+<script>
+    $(document).ready(function () {
+        $("#UPDATE #add_product_btn").one('click', function (event) {
+            event.preventDefault();
+            //do something
+            $(this).prop('disabled', true);
+        });
+    });
+</script>
+
+</html>
